8dcd32ca9f83b9a80c426137a704a2ac9d672857
suryachintu <suryaparsa@gmail.com>
2017-01-24 02:03:57 +0530

    Step : 1 Add Java Library to provide joke

---
+++ jokelibrary/src/main/java/com/example/JokeProvider.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class JokeProvider {
+
+    public static String getJoke(){
+        return "Be polite to every idiot you meet. He could be your boss tomorrow.";
+    }
+
+}
